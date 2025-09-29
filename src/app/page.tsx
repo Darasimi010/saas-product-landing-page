30ba94decc0a80201054ecf239b72bf9e92eeaf2
Oluwadarasimi Omojola <omojoladarasimi9@gmail.com>
2025-09-29 02:01:43 +0100

    completed the pricing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
+import { Pricing } from "@/sections/Pricing";
 import { ProductShowcase } from "@/sections/ProductShowcase";
 import { Header } from "@/sections/TheHeader";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <LogoTicker />
       <ProductShowcase />
+      <Pricing />
     </>
   );
 }
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -7,14 +7,14 @@ export const ProductShowcase = () => {
   return (
     <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <div className="max-w-[540px] mx-auto">
+        <div className="section-heading">
           <div className="flex justify-center">
             <div className="tag">Unlock your full potential</div>
           </div>
-          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+          <h2 className="section-title mt-5">
             Smarter tools for measurable progress
           </h2>
-          <p className="text-center text-[22px] leading-[30px] text-[#010D3E] tracking-tight mt-5">
+          <p className="section-description mt-5">
             Quickly transform your ideas into a powerful, responsive workspace
             without writing a single line of code. Build, launch, and manage
             with ease, all in one place.
