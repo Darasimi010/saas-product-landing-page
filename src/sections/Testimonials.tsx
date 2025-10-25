caaf489494d5b5fbed2ff894810a1a4d642cfece
Oluwadarasimi Omojola <omojoladarasimi9@gmail.com>
2025-10-26 05:31:44 +0800

    fixed lint warnings

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -101,7 +101,7 @@ const TestimonialsColumns = (props: {
       )}
     >
       {props.testimonials.map(({ text, imageSrc, name, username }) => (
-        <div key={username} className="card">
+        <div key={name} className="card">
           <div className="">{text}</div>
           <div className="flex gap-2 items-center mt-5">
             <Image
