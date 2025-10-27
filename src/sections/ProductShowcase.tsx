63601d1512595f8d357f2d0c1b35b7e32ce25827
Oluwadarasimi Omojola <omojoladarasimi9@gmail.com>
2025-10-27 10:51:11 +0800

    completed the animation for the productShowcase section of the page

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,24 @@
+"use client";
 import productImage from "@/assets/product-image.png";
 import tubeImage from "@/assets/tube.png";
 import pyramidImage from "@/assets/pyramid.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const productShowcaseRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: productShowcaseRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={productShowcaseRef}
+      className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container mx-auto">
         <div className="section-heading">
           <div className="flex justify-center">
@@ -23,19 +36,21 @@ export const ProductShowcase = () => {
         <div>
           <div className="relative">
             <Image src={productImage} alt="Product image" className="mt-10" />
-            <Image
-              src={pyramidImage}
+            <motion.img
+              src={pyramidImage.src}
               alt="Pyramid image"
               height={262}
               width={262}
               className="hidden md:block absolute -right-36 -top-28"
+              style={{ translateY }}
             />
-            <Image
-              src={tubeImage}
+            <motion.img
+              src={tubeImage.src}
               alt="Tube image"
               height={248}
               width={248}
               className="hidden md:block absolute -left-28 bottom-32"
+              style={{ translateY }}
             />
           </div>
         </div>
