e69adc61553774a36da74ae16985c8715ea0d4f4
Oluwadarasimi Omojola <omojoladarasimi9@gmail.com>
2025-10-27 09:17:49 +0800

    finished animating the hero section using motion.dev, formerly framer motion

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,27 @@
+"use client";
 import ArrowRight from "@/icons/ArrowRight";
 import cogImage from "@/assets/cog.png";
 import cylinderImage from "@/assets/cylinder.png";
 import noodleImage from "@/assets/noodle.png";
 import Image from "next/image";
+import { easeInOut, motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
 
 export const Hero = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
+    <section
+      ref={heroRef}
+      className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip"
+    >
       <div className="container mx-auto">
-        <div className="md:flex items-center lg:gap-80">
+        <div className="md:flex items-center lg:gap-60">
           <div className="md:w-[478px] lg:min-w-md">
             <div className="tag">Introducing version 3.2</div>
             <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
@@ -30,23 +43,32 @@ export const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image
-              src={cogImage}
+            <motion.img
+              src={cogImage.src}
               alt="Cog image"
               className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+              animate={{ translateY: [-30, 30] }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                ease: easeInOut,
+                duration: 3,
+              }}
             />
-            <Image
-              src={cylinderImage}
+            <motion.img
+              src={cylinderImage.src}
               alt="Cylinder image"
               width={220}
               height={220}
               className="hidden md:block -top-8 -left-32 md:absolute"
+              style={{ translateY: translateY }}
             />
-            <Image
-              src={noodleImage}
+            <motion.img
+              src={noodleImage.src}
               alt="Noodle image"
               width={220}
-              className="hidden lg:block lg:absolute top-[524px] left-[448px] rotate-[30deg]"
+              className="hidden lg:block lg:absolute top-[524px] left-[448px]"
+              style={{ rotate: 30, translateY: translateY }}
             />
           </div>
         </div>
